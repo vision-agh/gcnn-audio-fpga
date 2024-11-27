83a407500af18f38015cd51e0d6d6b4b0dc8cc33
Hiroshi NAKANO HN280727 <hn280727@gre051733.intra.cea.fr>
2024-11-27 17:09:48 +0100

    edit/make_hw_input.py

--- HW/src/generate_graph.sv
+++ HW/src/generate_graph.sv
@@ -11,18 +11,18 @@ module generate_graph #(
     input  logic            [F_WIDTH-1: 0]    f,
     input  logic                              is_valid,
 
-    output graph_event_type                   out_event,
-    output graph_edge_type  [MAX_EDGES-1 : 0] out_edges,
-    output logic            [T_WIDTH-1:0]     t_features,
-    output logic            [F_WIDTH-1:0]     f_features,
+    output graph_event_type                   event_to_normalize,
+    output graph_edge_type  [MAX_EDGES-1 : 0] edges_to_normalize,
+    output logic            [T_WIDTH-1:0]     t_feature_to_normalize,
+    output logic            [F_WIDTH-1:0]     f_feature_to_normalize,
     output logic            [N_WIDTH-1:0]     n,
     output logic                              empty
 );
 
     graph_event_type                    event_to_normalize;
     graph_edge_type   [MAX_EDGES-1 : 0] edges_to_normalize;
-    logic             [T_WIDTH-1 : 0]   t_feature_to_normalize;
-    logic             [F_WIDTH-1 : 0]   f_feature_to_normalize;
+//    logic             [T_WIDTH-1 : 0]   t_feature_to_normalize;
+//    logic             [F_WIDTH-1 : 0]   f_feature_to_normalize;
 
     edges_gen #(
     ) u_edges_gen (
@@ -40,18 +40,18 @@ module generate_graph #(
     );
     
     // for now path through module
-    normalize #(
-    ) u_normalize       (
-        .clk            ( clk                    ),
-        .reset          ( reset                  ),
-        .in_event       ( event_to_normalize     ),
-        .in_edges       ( edges_to_normalize     ),
-        .in_t_features  ( t_feature_to_normalize ),
-        .in_f_features  ( f_feature_to_normalize ),
-        .out_event      ( out_event              ),
-        .out_edges      ( out_edges              ),
-        .out_t_features ( t_features             ),
-        .out_f_features ( f_features             )
-    );
+//    normalize #(
+//    ) u_normalize       (
+//        .clk            ( clk                    ),
+//        .reset          ( reset                  ),
+//        .in_event       ( event_to_normalize     ),
+//        .in_edges       ( edges_to_normalize     ),
+//        .in_t_features  ( t_feature_to_normalize ),
+//        .in_f_features  ( f_feature_to_normalize ),
+//        .out_event      ( out_event              ),
+//        .out_edges      ( out_edges              ),
+//        .out_t_features ( t_features             ),
+//        .out_f_features ( f_features             )
+//    );
 
 endmodule
