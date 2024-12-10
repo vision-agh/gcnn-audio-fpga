fbe6c43de635673752a4ee183783fc9cfae7cb77
Piotr Wzorek <pwzorek@agh.edu.pl>
2024-12-10 14:42:05 +0100

    Graph_gen: Update and test for single sample
    Signed-off-by: Piotr Wzorek <pwzorek@agh.edu.pl>

--- HW/src/generate_graph.sv
+++ HW/src/generate_graph.sv
@@ -19,40 +19,18 @@ module generate_graph #(
     output logic                              empty
 );
 
-    graph_event_type                    event_to_normalize;
-    graph_edge_type   [MAX_EDGES-1 : 0] edges_to_normalize;
-
-    logic             [T_WIDTH-1 : 0]   t_feature_to_normalize;
-    logic             [F_WIDTH-1 : 0]   f_feature_to_normalize;
-
-    edges_gen #(
-    ) u_edges_gen (
-        .clk           ( clk                    ),
-        .reset         ( reset                  ),
-        .t             ( t                      ),
-        .f             ( f                      ),
-        .is_valid      ( is_valid               ),
-        .out_event     ( event_to_normalize     ),
-        .out_edges     ( edges_to_normalize     ),
-        .t_feature     ( t_feature_to_normalize ),
-        .f_feature     ( f_feature_to_normalize ),
-        .n             ( n                      ),
-        .empty         ( empty                  )
-    );
-    
-// for now path through module 
-    normalize #(
-    ) u_normalize       (
-        .clk            ( clk                    ),
-        .reset          ( reset                  ),
-        .in_event       ( event_to_normalize     ),
-        .in_edges       ( edges_to_normalize     ),
-        .in_t_feature   ( t_feature_to_normalize ),
-        .in_f_feature   ( f_feature_to_normalize ),
-        .out_event      ( out_event              ),
-        .out_edges      ( out_edges              ),
-        .out_t_feature  ( t_feature              ),
-        .out_f_feature  ( f_feature              )
+    edges_gen #() u_edges_gen (
+        .clk           ( clk           ),
+        .reset         ( reset         ),
+        .t             ( t             ),
+        .f             ( f             ),
+        .is_valid      ( is_valid      ),
+        .out_event     ( out_event     ),
+        .out_edges     ( out_edges     ),
+        .t_feature     ( t_feature     ),
+        .f_feature     ( f_feature     ),
+        .n             ( n             ),
+        .empty         ( empty         )
     );
 
 endmodule
---
+++ HW/tb/from_file__gen_graph_ut.sv
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+
+import graph_pkg::*;
+
+module gen_from_file_ut;
+
+    parameter MAX_X_COORD = 120;
+    parameter MAX_Y_COORD = 100;
+    parameter INPUT_PATH = "/home/pwz/Documents/SAMOLOT/TEST_AUDIO/audio.txt";
+    parameter OUTPUT_PATH = "/home/pwz/Documents/SAMOLOT/TEST_AUDIO/OUTPUT_GRAPH.txt";
+    parameter NS_PER_CLK = 5; // 250MHz is 4 clk every ns
+    parameter TIME_WINDOW = 1000000; // We test only single time window
+
+    logic [T_WIDTH-1:0] t;
+    logic [F_WIDTH-1:0] f;
+    logic is_valid;
+    event_type out_event;
+    edge_type [MAX_EDGES-1:0] out_edges;
+    logic empty;
+    logic [T_WIDTH-1:0] t_feature;
+    logic [F_WIDTH-1:0] f_feature;
+    logic [T_WIDTH-1:0] t_feature_reg;
+    logic [F_WIDTH-1:0] f_feature_reg;
+
+    logic clk;
+    logic rst;
+
+    // Queues with values from file
+	logic [T_WIDTH : 0]  f_coords [$];
+	logic [F_WIDTH : 0]  t_coords [$];
+
+    // Input and output files handler, scheduler.
+    int            cnt = 0;
+    int            file;
+    int            file_out;
+    string         line;
+    int            current_time_ns = 0;
+    string         t_string;
+    string         f_string;
+
+    initial begin
+        file = $fopen(INPUT_PATH, "r");
+        file_out = $fopen(OUTPUT_PATH, "w");
+
+        while(!$feof(file)) begin
+            $fgets(line, file);
+            $sscanf (line, "%s %s %s %s", t_string, f_string);
+            
+            // Save coordinates
+            t_coords.push_back(t_string.atoi());
+            f_coords.push_back(f_string.atoi());
+
+        end
+        $fclose(file);
+
+        // Get first values from queue
+        t_feature_reg   = t_coords.pop_front();
+        f_feature_reg   = f_coords.pop_front();
+
+        while(1) begin
+            if (cnt<10) begin
+                rst <= 1'b1;
+                cnt = cnt + 1;
+            end
+            else begin
+                rst <= 1'b0;
+            end
+            #1 clk <= 1'b0;
+            #1 clk <= 1'b1;
+        end
+    end
+
+    always @(posedge clk) begin
+        if (!rst) begin
+            
+            // Caluclate simulation time
+            current_time_ns = current_time_ns + NS_PER_CLK;
+            
+            // Put values on input whenever the timestamp is smaller than simultation time
+            if (t_feature_reg * 1000 < current_time_ns && t_feature_reg <= TIME_WINDOW) begin
+                is_valid <= 1;
+                t_feature_reg   <= t_coords.pop_front();
+                f_feature_reg   <= f_coords.pop_front();
+            end
+            else begin
+                 is_valid <= 0;
+            end
+
+            t <= t_feature_reg;
+            f <= f_feature_reg;
+
+            // Write outputs to file
+//            if (pos_item.valid) begin
+//                $fdisplay(file_out, "%0d %0d %0d %0d", pos_item.x, pos_item.y, pos_item.t, pos_item.p);
+//                for (int i = 0; i < graph_pkg::MAX_EDGES; i=i+1) begin
+//                    if (edges[i].is_connected) begin
+//                        $fdisplay(file_out, "EDGE_%0d = %0d %0d %0d", i, edges[i].is_connected, edges[i].t, edges[i].attribute);
+//                    end                
+//                end
+//            end
+
+//            // Finish simulation after 50.1 ms
+//            if (current_time_ns > 1500000) begin
+//                $fclose(file_out);
+//                $finish;
+//            end
+        end
+    end
+
+    generate_graph uut (
+        .clk(clk),
+        .reset(rst),
+        .t(t),
+        .f(f),
+        .is_valid(is_valid),
+        .out_event(out_event),
+        .out_edges(out_edges)
+        //.t_feature(t_feature),
+        //.f_feature(f_feature)
+    );
+
+endmodule
